64b53a6b14fd0476d0f81d27f79602c5b61dff01
Marcus Franke <marcus.franke@gmail.com>
2025-01-14 09:49:31 +0100

    chore: code formatting fixed

--- main.go
+++ main.go
@@ -1,41 +1,62 @@
 package main
 
 import (
-  "log"
+	"log"
 
-  "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbletea"
 )
 
-type model struct {}
+type model struct {
+	questions   []string
+	width       int
+	height      int
+	answerField textinput.Model
+}
+
+func New(questions []string) *model {
+	return &model{
+		questions: questions,
+	}
+}
 
 func (m model) Init() tea.Cmd {
-  return nil
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-  switch msg := msg.(type) {
-  case tea.KeyMsg:
-    switch msg.String() {
-    case "ctrl+c":
-      return m, tea.Quit
-    }
-  }
-  return m, nil
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
 }
 
 func (m model) View() string {
-  return "Placeholder"
-} 
+	if m.width == 0 {
+		return "loading"
+	}
+	return "Placeholder"
+}
 
 func main() {
-  f, err := tea.LogToFile("debug.log", "debug")
-  if err != nil {
-    log.Fatalf("err: %w", err)
-  }
-  defer f.Close()
+	questions := []string{"What is the name of your cluster", "How many control plane nodes?", "How many worker nodes?"}
+	m := New(questions)
 
-  p := tea.NewProgram(model{}, tea.WithAltScreen())
-  if _, err := p.Run(); err != nil {
-    log.Fatal(err)
-  }
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		log.Fatalf("err: %w", err)
+	}
+	defer f.Close()
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
