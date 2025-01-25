5e7e9f9f5d84f751aaf94a650f6b4e921e71c3d2
Marcus Franke <marcus.franke@gmail.com>
2025-01-25 13:07:49 +0100

    chore: apply style to input field

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-var (
-	style = lipgloss.NewStyle().Padding(1, 2)
-)
+type Styles struct {
+	BorderColor lipgloss.Color
+	TextColor   lipgloss.Color
+	InputField  lipgloss.Style
+}
 
 type model struct {
 	questions   []string
@@ -18,21 +20,38 @@ type model struct {
 	height      int
 	index       int
 	answerField textinput.Model
+	styles      *Styles
 }
 
 func New(questions []string) *model {
 	answerField := textinput.New()
 	answerField.Placeholder = "Enter your answer"
 	answerField.Focus()
+
+	styles := DefaultStyles()
+
 	return &model{
 		questions:   questions,
 		answerField: answerField,
 		index:       0,
+		styles:      styles,
 	}
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
+}
+
+func DefaultStyles() *Styles {
+	return &Styles{
+		BorderColor: lipgloss.Color("#000000"),
+		TextColor:   lipgloss.Color("#ffffff"),
+		InputField: lipgloss.NewStyle().
+			Border(lipgloss.InnerHalfBlockBorder(), false, false, false, true).
+			BorderForeground(lipgloss.Color("#000000")).
+			Padding(1, 2).
+			Width(100),
+	}
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -53,7 +72,11 @@ func (m model) View() string {
 	if m.width == 0 {
 		return "loading"
 	}
-	return lipgloss.JoinVertical(lipgloss.Center, m.questions[m.index], m.answerField.View())
+	return lipgloss.JoinVertical(
+		lipgloss.Center,
+		m.questions[m.index],
+		m.styles.InputField.Render(m.answerField.View()),
+	)
 }
 
 func main() {
