2c628f2da92feb4f7d2a6ebceaf11b3f2449de04
Marcus Franke <marcus.franke@gmail.com>
2025-01-25 14:48:46 +0100

    feat: make example workable
    Iterate over all questions, store the answers and end the program
    while writing the collected answers to the terminal.
    
    This should be a building block, for creating a management tool,
    like envisioned in te readme file.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/charmbracelet/bubbles/textinput"
@@ -15,15 +16,21 @@ type Styles struct {
 }
 
 type model struct {
-	questions   []string
+	done        bool
+	index       int
 	width       int
 	height      int
-	index       int
+	questions   []Question
 	answerField textinput.Model
 	styles      *Styles
 }
 
-func New(questions []string) *model {
+type Question struct {
+	question string
+	answer   string
+}
+
+func New(questions []Question) *model {
 	answerField := textinput.New()
 	answerField.Placeholder = "Enter your answer"
 	answerField.Focus()
@@ -35,11 +42,24 @@ func New(questions []string) *model {
 		answerField: answerField,
 		index:       0,
 		styles:      styles,
+		done:        false,
 	}
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
+}
+
+func (m *model) Next() {
+	if m.index < len(m.questions)-1 {
+		m.index++
+		m.answerField.SetValue("")
+		m.answerField.Focus()
+	} else {
+		m.index = 0
+		m.answerField.SetValue("done")
+		m.answerField.Blur()
+	}
 }
 
 func DefaultStyles() *Styles {
@@ -55,6 +75,9 @@ func DefaultStyles() *Styles {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	current := &m.questions[m.index]
+
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
@@ -63,14 +86,30 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c":
 			return m, tea.Quit
+		case "enter":
+			if m.index == len(m.questions)-1 {
+				m.done = true
+			}
+			current.answer = m.answerField.Value()
+			m.answerField.SetValue("")
+			m.Next()
+			return m, nil
 		}
 	}
-	return m, nil
+	m.answerField, cmd = m.answerField.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
 	if m.width == 0 {
 		return "loading"
+	}
+	if m.done {
+		var output string
+		for _, question := range m.questions {
+			output += fmt.Sprintf("%s: %s\n", question.question, question.answer)
+		}
+		return output
 	}
 	return lipgloss.Place(
 		m.width,
@@ -79,14 +118,18 @@ func (m model) View() string {
 		lipgloss.Center,
 		lipgloss.JoinVertical(
 			lipgloss.Center,
-			m.questions[m.index],
+			m.questions[m.index].question,
 			m.styles.InputField.Render(m.answerField.View()),
 		),
 	)
 }
 
 func main() {
-	questions := []string{"What is the name of your cluster", "How many control plane nodes?", "How many worker nodes?"}
+	questions := []Question{
+		{question: "What is the name of your cluster", answer: ""},
+		{question: "How many control plane nodes?", answer: ""},
+		{question: "How many worker nodes?", answer: ""},
+	}
 	m := New(questions)
 
 	f, err := tea.LogToFile("debug.log", "debug")
